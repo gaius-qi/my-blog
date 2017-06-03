13fbaf54518e9b1110d7c269745c3c565a605485
“qwbtc” <bob.dylan0313@gmail.com>
2017-06-03 17:35:11 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="container-all">
-  <!-- 应该放入/static文件夹中，因为vue-cli配置webpack不参与打包 -->
+  <!-- 应该放入/static文件夹中.因为vue-cli配置webpack不参与打包 -->
   <div class="header-container" ref='header'>
     <video autoplay loop poster="/static/image/back.png" id="bgvid">
       <source src="/static/video/background.mp4" type="video/mp4">
