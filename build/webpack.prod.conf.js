01f669665c258aadba63f5c5aaa44331f521ef08
“qwbtc” <bob.dylan0313@gmail.com>
2018-04-30 20:17:27 +0800

    add prerender-spa-plugin solve the SPA's SEO

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -27,6 +28,13 @@ var webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
+    // prerender-spa-plugin插件用于在单页应用中预渲染静态html内容
+    new PrerenderSpaPlugin(
+      // 输出目录的绝对路径
+      path.join(__dirname, '../dist'),
+      // 预渲染的路由
+      [ '/', '/cv' ]
+    ),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
